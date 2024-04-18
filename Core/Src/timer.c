5c77dae3953e11f64cefb83915791e039348d0dd
Joon65 <joondong.lee@outlook.com>
2024-04-18 15:16:20 +0900

    CollisionAvoidance_v0.1 - Basic environment setup on Nucleo-H743ZI2

---
+++ Core/Src/timer.c
@@ -0,0 +1,18 @@
+/*
+ * timer.c
+ *
+ *  Created on: Apr 17, 2024
+ *      Author: joond
+ */
+
+#include "main.h"
+#include "stm32h7xx_hal.h"
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim->Instance == TIM6)
+	{
+		// timer = 1ms
+		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+	}
+}
